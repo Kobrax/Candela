00a01f831d6482800d732bd24817fee93cafc940
Kobrax <adam3308@stud.kea.dk>
2016-05-19 15:35:04 +0200

    new class

---
+++ src/com/Controller/LotTableController.java
@@ -0,0 +1,89 @@
+package com.Controller;
+
+import com.Model.LoginModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Created by Dom on 2016-05-19.
+ */
+public class LotTableController {
+
+    public static ObservableList<ObservableList> data;
+    public TableView tableView;
+    private  Connection conn;
+
+    public void createTable(String loggedUs) {
+        data = FXCollections.observableArrayList();
+        try {
+
+            String DB_URL = "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7119030";
+            String USER = "sql7119030";
+            String PASS = "ira72lBrrp";
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            String sql = "SELECT lotNumber, realEstateRegister, area FROM lot WHERE username = ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, loggedUs);
+
+            ResultSet result = preparedStatement.executeQuery();
+
+
+// create coulmn
+            for (int i = 0; i < result.getMetaData().getColumnCount(); i++) {
+// dynamic table coulmns
+                final int j = i;
+                TableColumn colLot = new TableColumn(result.getMetaData().getColumnName(i + 1));
+
+                colLot.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+//Add comlumn to lot
+
+                tableView.getColumns().addAll(colLot);
+            }
+
+
+// fill data of Tables rows
+
+            while (result.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+
+                for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(result.getString(i));
+                }
+
+                data.add(row);
+            }
+
+
+            // Add  data TableView
+            tableView.setItems(data);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Database problem");
+
+        }
+    }
+
+    public void resetTable()
+    {
+        data.removeAll(data);
+        tableView.getColumns().clear();
+    }
+}
