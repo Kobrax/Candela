81bd7cf57cd17740d96b13c3db71f4f9a1966a25
mazurmazurmazur <grzaneczka@gmail.com>
2016-05-20 10:26:37 +0200

    filling textfields in progress

---
+++ src/com/Controller/DisplayLotController.java
@@ -0,0 +1,7 @@
+package com.Controller;
+
+/**
+ * Created by MazurJestBoski on 2016-05-20.
+ */
+public class DisplayLotController {
+}
---
+++ src/com/Model/DisplayLot.java
@@ -0,0 +1,7 @@
+package com.Model;
+
+/**
+ * Created by MazurJestBoski on 2016-05-20.
+ */
+public class DisplayLot {
+}
