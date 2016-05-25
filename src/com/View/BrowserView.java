1774beb3ed4886d0845c0b6d3ea28738a4a92d2f
Kobrax <adam3308@stud.kea.dk>
2016-05-25 13:44:50 +0200

    work?

---
+++ src/com/View/BrowserView.java
@@ -0,0 +1,32 @@
+package com.View;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+/**
+ * Created by Dom on 2016-05-25.
+ */
+public class BrowserView {
+    public void start()
+    {
+        Stage browserStage = new Stage();
+        WebView view = new WebView();
+        Scene scene = new Scene(view, 900, 900);
+        WebEngine webEngine = view.getEngine();
+
+
+        String url = "https://kolobrzeski.webewid.pl/is2/iEwid/";
+
+        view.isResizable();
+        webEngine.load(url);
+
+
+        browserStage.setScene(scene);
+        browserStage.show();
+
+
+    }
+}
